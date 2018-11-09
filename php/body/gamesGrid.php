c9860da7d05adf012070d688e52ff2e54cee7d84
ElTitoBadi <artur.bcn1998@gmail.com>
2018-11-10 00:25:44 +0100

    Grid de juegos

---
+++ php/body/gamesGrid.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>
+        Riddle
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 p-3">
+                <div class="card rounded-0">
+                    <img class="card-img-top rounded-0" src="picGame1.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Ataque a Pearl Harbor</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card's content.</p>
+                        <a href="#" class="btn btn-primary float-right">Comenzar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 p-3">
+                <div class="card rounded-0">
+                    <img class="card-img-top" src="descarga.svg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card's content.</p>
+                        <button type="button" class="btn btn-secondary float-right" disabled>Próximamente</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 p-3">
+                <div class="card rounded-0">
+                    <img class="card-img-top" src="descarga.svg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick
+                            examplee xampleexam pleexampleex ampleexample exampleexampleexample exampleexam
+                            pleexampleexa mpleexampleexampleexample
+                            text to build on the card title and make up the bulk of
+                            the
+                            card's content.</p>
+                        <button type="button" class="btn btn-secondary float-right" disabled>Próximamente</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4    col-lg-4 col-md-6 col-sm-12 p-3">
+                <div class="card rounded-0">
+                    <img class="card-img-top" src="descarga.svg" alt="Card image cap">
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
