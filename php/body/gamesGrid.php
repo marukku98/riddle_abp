01570b395f96ca4c32ce3a0b225996ccbf3b4cea
ElTitoBadi <artur.bcn1998@gmail.com>
2018-11-10 00:31:02 +0100

    a

--- php/body/gamesGrid.php
+++ php/body/gamesGrid.php
@@ -61,7 +61,11 @@
                 <div class="card rounded-0">
                     <img class="card-img-top" src="descarga.svg" alt="Card image cap">
                     <div class="card-body">
-
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card's content.</p>
+                        <button type="button" class="btn btn-secondary float-right" disabled>Próximamente</button>
                     </div>
                 </div>
             </div>
