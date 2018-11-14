6bd872249ce3ee0ac8182bc6be2774fe749c740a
CEP <galcera@hotmail.com>
2018-11-14 11:23:44 +0100

    Update selector d'enigmes

--- php/body/game.php
+++ php/body/game.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="/riddle_abp/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/riddle_abp/assets/css/game.css">
     <script src="/riddle_abp/assets/js/jquery-3.3.1.min.js"></script>
     <script src="/riddle_abp/assets/js/popper.min.js"></script>
     <script src="/riddle_abp/assets/js/bootstrap.min.js"></script>
@@ -17,98 +18,67 @@
 </head>
 
 <body>
-<header>
-    <nav class="navbar navbar-expand-lg" style="background-color: #32CBED;">
-      <a class="navbar-brand" href="index.php">
-        <div class="page-header">
-          <!-- <h1>RIDDLE</h1> -->
-          <img src="/riddle_abp/assets/img/titulo.png" style="max-height: 200px; max-width: 200px;" alt="">
-        </div>
-      </a>
-      <div class="collapse navbar-collapse" id="navbarColor02">
-        <ul class="navbar-nav mr-auto"></ul>
-        <a class="navbar-brand">
-          <h5><a class="text-white" href="login.php">Iniciar Sesion</a> </h5>
-        </a>
-        <div class="vertical-divider"></div>
-        <a class="navbar-brand">
-          <h5><a class="text-white" href="register.php">Registrate</a> </h5>
-        </a>
-      </div>
-    </nav>
+    <header>
+        <nav class="navbar navbar-expand-lg" style="background-color: #32CBED;">
+            <a class="navbar-brand" href="index.php">
+                <div class="page-header">
+                    <!-- <h1>RIDDLE</h1> -->
+                    <img src="/riddle_abp/assets/img/titulo.png" style="max-height: 200px; max-width: 200px;" alt="">
+                </div>
+            </a>
+            <div class="collapse navbar-collapse" id="navbarColor02">
+                <ul class="navbar-nav mr-auto"></ul>
+                <a class="navbar-brand">
+                    <h5>
+                        <a class="text-white" href="login.php">Iniciar Sesion</a>
+                    </h5>
+                </a>
+                <div class="vertical-divider"></div>
+                <a class="navbar-brand">
+                    <h5>
+                        <a class="text-white" href="register.php">Registrate</a>
+                    </h5>
+                </a>
+            </div>
+        </nav>
 
-    <div id="navigation">
-      <nav class="navbar navbar-expand-lg navbar-light py-1" style="background-color: #45B8D2;">
-        <div class="navbar-collapse collapse navbars" id="navbar2">
-          <ul class="navbar-nav">
-            <li class="nav-item ">
-              <a class="nav-link col-2 text-white" href="">MENU</a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link col-2 text-white" href="gamesGrid.php">JUEGOS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link col-2 text-white" href="contact.php">CONTACTO</a>
-            </li>
-          </ul>
+        <div id="navigation">
+            <nav class="navbar navbar-expand-lg navbar-light py-1" style="background-color: #45B8D2;">
+                <div class="navbar-collapse collapse navbars" id="navbar2">
+                    <ul class="navbar-nav">
+                        <li class="nav-item ">
+                            <a class="nav-link col-2 text-white" href="">MENU</a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link col-2 text-white" href="gamesGrid.php">JUEGOS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link col-2 text-white" href="contact.php">CONTACTO</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
-      </nav>
+
+    </header>
+
+    <div class="game ">
+            <div class="gameText">
+                <h5 class="m-0">Nivel 1</h5>
+                <p>Preparativos para la batalla</p>
+            </div>
+    
+        </div>
+
+
+    <div class="game lockedImg">
+        <div class="gameText">
+            <h5 class="m-0">Nivel 1</h5>
+            <p>Preparativos para la batalla</p>
+        </div>
+        <div class="lockHover"></div>
+
     </div>
-
-  </header>
-    <div class="container">
-        <div class="row">
-
-            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 p-3">
-                <div class="card rounded-0">
-                    <img class="card-img-top rounded-0" src="/riddle_abp/assets/img/picGame1.jpg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Ataque a Pearl Harbor</h5>
-                        <p class="card-text">Revive el ataque militar de Japón contra Estados Unidos en una base naval en Hawaii.</p>
-                        <a href="game.php" class="btn btn-primary float-right">Comenzar</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 p-3">
-                <div class="card rounded-0">
-                    <img class="card-img-top" src="/riddle_abp/assets/img/picGame1.jpg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the
-                            card's content.</p>
-                        <button type="button" class="btn btn-secondary float-right" disabled>Próximamente</button>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 p-3">
-                <div class="card rounded-0">
-                    <img class="card-img-top" src="/riddle_abp/assets/img/picGame1.jpg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the
-                            card's content.</p>
-                        <button type="button" class="btn btn-secondary float-right" disabled>Próximamente</button>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-4    col-lg-4 col-md-6 col-sm-12 p-3">
-                <div class="card rounded-0">
-                    <img class="card-img-top" src="/riddle_abp/assets/img/picGame1.jpg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the
-                            card's content.</p>
-                        <button type="button" class="btn btn-secondary float-right" disabled>Próximamente</button>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </div>
-
 </body>
 
 </html>
