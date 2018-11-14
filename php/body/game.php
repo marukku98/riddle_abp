54bcad9b07d8f79c6cdc2bccbd20cdd51503b568
CEP <galcera@hotmail.com>
2018-11-14 11:46:10 +0100

    Update
    Update de gridGames, selector d'enigmes.

--- php/body/game.php
+++ php/body/game.php
@@ -62,23 +62,66 @@
 
     </header>
 
-    <div class="game ">
-            <div class="gameText">
-                <h5 class="m-0">Nivel 1</h5>
-                <p>Preparativos para la batalla</p>
-            </div>
-    
-        </div>
-
-
-    <div class="game lockedImg">
-        <div class="gameText">
-            <h5 class="m-0">Nivel 1</h5>
-            <p>Preparativos para la batalla</p>
-        </div>
-        <div class="lockHover"></div>
-
+    <div class="container title">
+        <h2>Attack on Pearl Harbor</h2>
     </div>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 p-3">
+
+                <a href="enigma1.php">
+                <div class="game">
+                    <div class="gameText">
+                        <h5 class="m-0">Nivel 1</h5>
+                        <p>Preparativos para la batalla</p>
+                    </div>
+                </div>
+                </a>
+
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 p-3">
+                <div class="game lockedImg">
+                    <div class="gameText">
+                        <h5 class="m-0">Nivel 2</h5>
+                        <p>Primera oleada</p>
+                    </div>
+                    <div class="lockHover"></div>
+
+                </div>
+
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 p-3">
+                <div class="game lockedImg">
+                    <div class="gameText">
+                        <h5 class="m-0">Nivel 3</h5>
+                        <p>Segunda oleada</p>
+                    </div>
+                    <div class="lockHover"></div>
+
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 p-3">
+                <div class="game lockedImg">
+                    <div class="gameText">
+                        <h5 class="m-0">Nivel 4</h5>
+                        <p>Las consequencias</p>
+                    </div>
+                    <div class="lockHover"></div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
 </body>
 
 </html>
