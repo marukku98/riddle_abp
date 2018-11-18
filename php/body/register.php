93905b882005141ade428949646bd3c71945fd16
ElTitoBadi <artur.bcn1998@gmail.com>
2018-11-18 19:38:20 +0100

    error feedback register

--- php/body/register.php
+++ php/body/register.php
@@ -1,4 +1,5 @@
-<?php require_once "../templates/user.php" ?>
+<?php session_start();
+ require_once "../templates/user.php" ?>
 
 <?php startblock("titulo"); ?>
  Registro
@@ -7,7 +8,18 @@
 <?php startblock("principal"); ?>
    <div class="card" style="width: 20rem;">
        <div class="card-body">
-           <h4 class="card-title">Crea tu cuenta</h4><br>
+           <h4 class="card-title">Crea tu cuenta</h4>
+
+           
+           <?php 
+                if(isset($_SESSION['error'])){
+                    echo "<p class='error mb-1 ml-1'>".$_SESSION['error']."</p>";
+                    unset($_SESSION['error']);
+                }
+                else{
+                    echo '</br>';
+                }
+            ?>
 
            <form action="/riddle_abp/php/conexion/register.php" method="POST">
                <div class="form-group row">                      
