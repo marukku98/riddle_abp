c23b3b1e4a8c4d2615f48b1496b3f70bb1d2d378
CEP <galcera@hotmail.com>
2018-11-22 12:14:28 +0100

    update enigma3

--- assets/js/enigma3.js
+++ assets/js/enigma3.js
@@ -1,21 +1,87 @@
-var flota = [
-    [1, 2, 3],
-    [97, 98, 99]
-];
+var cas=[];
+var barcos=[];
+
+for (var i = 0; i < 100; i++) {
+    cas[i]=({pos:i,barco:false,tocado:false});    
+}
+
+cas[0]['barco']=true;
+cas[55]['barco']=true;
+
+function a(){
+    alert (cas[50]['pos']);
+}
+
+function colocarBarco(len){
+    var orientacio = Math.round(Math.random());
+    var vertical = 0;
+    var choca = false;
+    var x;
+    var y;
+    var barco = [];
+
+    do {
+        if(orientacio === vertical){
+            x = Math.floor(Math.random()*(10-len));
+            y = Math.floor(Math.random()*(10));
+            for (var i = 0; i < len; i++) {
+                if(CheckPos(x+i,y)){
+                   choca=true;
+                }                
+            }
+            if(!choca){
+                for (var i = 0; i < len; i++) {
+                    barco[i]=(x+i)*10+y;
+                }               
+            }
+        }else{
+            x = Math.floor(Math.random()*(10));
+            y = Math.floor(Math.random()*(10-len));
+            for (var i = 0; i < len; i++) {
+                if(CheckPos(x,y+i)){
+                   choca=true;
+                }                
+            }
+            if(!choca){
+                for (var i = 0; i < len; i++) {
+                    barco[i]=x*10+(y+i);
+                }                
+            }
+        }
+    } while (choca === true);
+
+    return barco;    
+}
 
 function disparar(pos) {
     ocultarBoton(pos);
-    array.forEach(element => {
-
-    });
 }
 
 function ocultarBoton(pos) {
-    $("#" + pos).css({
-        "border-radius": "50%",
-        "background": "red",
-        "height": "25px",
-        "width": "25px",
-        "transform": "translate(50%,50%)"
-    });
+    if(cas[pos]['barco'] === true){
+        $("#" + pos).css({
+            "border-radius": "50%",
+            "background": "red",
+            "height": "25px",
+            "width": "25px",
+            "transform": "translate(50%,50%)"
+        });
+    }else{
+        $("#" + pos).css({
+            "border-radius": "50%",
+            "background": "white",
+            "height": "25px",
+            "width": "25px",
+            "transform": "translate(50%,50%)"
+        });
+    }
+}
+
+function CheckPos(x,y){
+    var bool = false;
+    var pos = x*10+y;
+    if(cas[pos]['barco']===true){
+        bool = true;
+    }
+    return bool;
 }
--- php/body/enigma3.php
+++ php/body/enigma3.php
@@ -1,52 +1,19 @@
 <?php require_once "../templates/master.php" ?>
 
 <script src="/riddle_abp/assets/js/enigma3.js"></script>
+<link rel="stylesheet" href="/riddle_abp/assets/css/enigma3.css">
 
-<?php startblock("titulo"); ?>
-Enigma 1
+<?php startblock("titulo"); ?> Enigma 1
 <?php endblock(); ?>
 
 <?php startblock("principal"); ?>
-<style>
-    .box {
-        background-color: #808080;
-        position: absolute;
-        height: 50px;
-        width: 50px;
-        border: solid 1px #a5a5a5;
-    }
-
-    .box:hover {
-        background-color: #a5a5a5;
-    }
-
-    .sea {
-        position: relative;
-        background-size: cover;
-
-        background-image: url("/riddle_abp/assets/img/sea.png");
-        height: 550px;
-        width: 550px;
-    }
-
-    .block {
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        z-index: 2;
-    }
-</style>
 
 <body>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col6">
-                <div class="sea mt-1 mr-1">
-                    <div class="block"></div>
-                    <?php
+            <div class="sea mt-1">
+                <div class="block"></div>
+                <?php
                 for ($i=0; $i < 10; $i++) { 
                     for ($j=0; $j < 10; $j++) { 
                         echo '<button class="box" 
@@ -57,14 +24,10 @@ Enigma 1
                     } 
                 }
             ?>
-                </div>
             </div>
-            <div class="col6 mt-1 ml-1">
-                <div class="sea">
-
-                </div>
-            </div>
+            <div class="sea mt-1"></div>
         </div>
+    </div>
     </div>
 
 
